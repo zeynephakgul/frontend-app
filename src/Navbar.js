9b6887f7c367716c5346366a1b3c963ea0d87dfb
Zeynep <zeynep.h.akgul@gmail.com>
2024-07-05 12:04:42 +0300

    navbar update

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,17 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const handleLogout = () => {
+        // Clear session data, e.g., localStorage.clear();
+        // Redirect to login page
+        window.location.href = '/'; // This will reload the page and redirect to /login
+    };
+
     return (
-        <nav style={{ backgroundColor: '#333', color: '#fff', padding: '10px', marginBottom: '20px' }}>
+        <nav style={{ backgroundColor: '#333', color: '#fff', padding: '10px', marginBottom: '20px', borderRadius: '8px' }}>
             <ul style={{ display: 'flex', justifyContent: 'space-around', listStyleType: 'none', margin: 0, padding: 0 }}>
                 <li>
-                    <Link to="/profile" style={{ color: '#fff', textDecoration: 'none' }}>Profile</Link>
+                    <Link to="/mainpage" style={{ color: '#fff', textDecoration: 'none' }}>Main Page</Link>
                 </li>
                 <li>
                     <Link to="/transactions" style={{ color: '#fff', textDecoration: 'none' }}>Transactions</Link>
                 </li>
                 <li>
                     <Link to="/transfers" style={{ color: '#fff', textDecoration: 'none' }}>Transfers</Link>
+                </li>
+                <li>
+                    <Link to="/profile" style={{ color: '#fff', textDecoration: 'none' }}>Profile</Link>
+                </li>
+                <li>
+                    <button onClick={handleLogout} style={{ backgroundColor: 'transparent', border: 'none', color: '#fff', cursor: 'pointer' }}>Logout</button>
                 </li>
             </ul>
         </nav>
